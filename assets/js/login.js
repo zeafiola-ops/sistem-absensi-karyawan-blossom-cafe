09f745400c676e299699c20bd70f417d1f32680c
zeafiola-ops <zeafiola@gmail.com>
2026-07-28 07:49:23 -0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,23 @@
-console.log("login.js berhasil dimuat");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const btnLogin = document.getElementById("btnLogin");
+btnLogin.addEventListener("click", function () {
+
+    if (username.value === "admin" && password.value === "12345") {
+
+        Swal.fire({
+            icon: "success",
+            title: "Login Berhasil",
+            text: "Selamat datang Admin!"
+        }).then(() => {
+            window.location.href = "pages/dashboard.html";
+        });
+
+    } else {
+
+        Swal.fire({
+            icon: "error",
+            title: "Login Gagal",
+            text: "Username atau Password salah!"
+        });
+
+    }
+
+});
