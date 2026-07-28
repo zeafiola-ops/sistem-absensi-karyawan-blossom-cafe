9a9ad563b27ef8dc3b5770418ff9dd2384a3b70f
zeafiola-ops <zeafiola@gmail.com>
2026-07-28 07:46:18 -0700

    Add login element references in login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,1 +1,4 @@
 console.log("login.js berhasil dimuat");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const btnLogin = document.getElementById("btnLogin");
